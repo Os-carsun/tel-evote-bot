51812b459c0ff1d8f3de529bbd93e5f63ad00370
jysun <jysun@cs.nctu.edu.tw>
2018-04-21 17:54:41 +0800

    update

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const Telegraf = require('telegraf')
 const { Markup } = Telegraf
 const session = require('telegraf/session')
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf('process.env.BOT_TOKEN')
 //const bot = new Telegraf('')
 bot.use(session())
 
@@ -21,12 +21,16 @@ bot.command('/vote', (ctx) => {
     ctx.session.is_vote = true
     ctx.session.options = []
     ctx.session.result = {}
-    const name = ctx.message.text.replace('/start_vote', '')
+    const name = ctx.message.text.replace('/vote', '')
 
-    if(name.length > 0) 
+    if(name.length > 0) {
+        ctx.session.vote_name = name
         ctx.reply('start a new vote ' + name)
-    else
+    }
+    else{
+        ctx.session.vote_name = 'vote'
         ctx.reply('start a new vote')    
+    }
 })
 
 
@@ -57,7 +61,7 @@ bot.hears(/vote/i, (ctx) => {
     const msg = ctx.message.text.replace('vote ', '')
     if(!msg) return 
     if(ctx.session.options.indexOf(msg) >= 0) {
-        let result = ""
+        let result = ctx.session.vote_name + ":\n"
         ctx.session.result[msg] = ~~ctx.session.result[msg] + 1
         for(let key of ctx.session.options) { 
             result += `${key}: ${ctx.session.result[key]?ctx.session.result[key]:0}\n`
@@ -72,7 +76,7 @@ bot.command('/done', (ctx) => {
         return
     }
     ctx.reply('end this vote', Markup.removeKeyboard().extra())
-    let result = ""
+    let result = ctx.session.vote_name + ":\n"
     for(let key of ctx.session.options) { 
         result += `${key}: ${ctx.session.result[key]?ctx.session.result[key]:0}\n`
     }
